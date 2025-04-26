7370ea723b458daefa0a8259eaa38616ff37652a
Jaime <jaime.rosco@gmail.com>
2025-04-26 16:57:22 +0200

    inicializar

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Módulo principal para la plataforma de trading algorítmico.
+"""
+
+def main():
+    """
+    Función principal que inicia la aplicación.
+    """
+    print("Hello World! Bienvenido a tu plataforma de Trading Algorítmico")
+
+if __name__ == "__main__":
+    main()
